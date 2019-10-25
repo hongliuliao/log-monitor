d6a994e73f64ba9f6c37dfe04c47cf5fef10faad
hongliuliao <357857613@qq.com>
2019-10-25 20:55:16 +0800

    remove simple-flow deps

--- dependency/simple-flow/dependency/ehttp/script/run_ut.sh
+++
@@ -1,6 +0,0 @@
-./output/bin/threadpool_test
-./output/bin/sim_parser_test
-./output/bin/string_utils_test
-./output/bin/simple_config_test
-./output/bin/simple_log_test
-./output/bin/epoll_socket_test
--- dependency/simple-flow/dependency/ehttp/src/ehttp_version.h
+++
@@ -1,6 +0,0 @@
-#ifndef EHTTP_VERSION_H
-#define EHTTP_VERSION_H
-
-#define EHTTP_VERSION "1.0.4"
-
-#endif
